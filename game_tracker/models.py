d8022bf4cb14578c96ec75d64c583222a00062c1
Carson Killbreath <kill4992@kettering.edu>
2015-12-28 23:51:18 -0500

    Added Character model.

--- game_tracker/models.py
+++ game_tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+    speed = models.IntegerField(default=0)
+    might = models.IntegerField(default=0)
+    sanity = models.IntegerField(default=0)
+    knowledge = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
