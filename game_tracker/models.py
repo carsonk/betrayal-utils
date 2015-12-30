542a4a7c767d395e3613ef5787243466ee750621
Carson Killbreath <kill4992@kettering.edu>
2015-12-30 00:09:48 -0500

    Loading images.

--- game_tracker/models.py
+++ game_tracker/models.py
@@ -4,10 +4,19 @@ from django.db import models
 
 class Character(models.Model):
     name = models.CharField(max_length=200)
-    speed = models.IntegerField(default=0)
-    might = models.IntegerField(default=0)
-    sanity = models.IntegerField(default=0)
-    knowledge = models.IntegerField(default=0)
+
+    image = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+
+    speed_options = models.CharField(max_length=200)
+    might_options = models.CharField(max_length=200)
+    sanity_options = models.CharField(max_length=200)
+    knowledge_options = models.CharField(max_length=200)
+
+    speed_index = models.IntegerField(default=0)
+    might_index = models.IntegerField(default=0)
+    sanity_index = models.IntegerField(default=0)
+    knowledge_index = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
--- game_tracker/views.py
+++ game_tracker/views.py
@@ -1,8 +1,54 @@
-from django.shortcuts import render
+import json
 
-from .models import Character
+from django.conf import settings
+from django.shortcuts import render, redirect
+from . models import Character
+
+import os
 
 def index(request):
     character_list = Character.objects.order_by('name')
     context = {'character_list': character_list, 'test': True}
     return render(request, 'game_tracker/index.html', context)
+
+def reset(request):
+    Character.objects.all().delete()
+
+    characters = None
+    characters_path = os.path.join(settings.GAME_DIR, 'data', \
+        'characters.json')
+    with open(characters_path) as f:
+        characters_json = f.read()
+        characters = json.loads(characters_json)
+
+    organized_characters = []
+
+    current_color = None
+    for character_set in characters:
+        current_color = character_set["color"]
+
+        for character in character_set["characters"]:
+            new_char = Character.objects.create()
+
+            new_char.name = character["name"]
+            new_char.color = current_color
+
+            new_char.image = character["name"].split()[0].lower() + ".png"
+
+            new_char.speed_options = character["speed"]
+            new_char.might_options = character["might"]
+            new_char.sanity_options = character["sanity"]
+            new_char.knowledge_options = character["knowledge"]
+
+            new_char.speed_index = character["default_speed"]
+            new_char.might_index = character["default_might"]
+            new_char.sanity_index = character["default_sanity"]
+            new_char.knolwedge_index = character["default_knowledge"]
+
+            new_char.save()
+
+    return redirect('index')
+
+def add(request):
+    context = {}
+    return render(request, 'game_tracker/add.html', context)
